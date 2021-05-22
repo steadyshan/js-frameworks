fe435d55d2a3057a802b6a10c01bdf45585c9194
Shantanu <shanwork2012@gmail.com>
2021-05-22 09:31:10 -0700

    v-museum - root and side nav components

---
+++ Angular_lte_4/v-museum/src/app/app-museum-intro/app-museum-intro.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+// import { SideNavBarComponent } from  'compo / './components/side-nav-bar/side-nav-bar.component';
+import { SideNavBarComponent } from '../components/side-nav-bar/side-nav-bar.component';
+
+@Component({
+  selector: 'app-museum-intro',
+  templateUrl: './app-museum-intro.component.html',
+  styleUrls: ['./app-museum-intro.component.css']
+})
+export class AppMuseumIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Angular_lte_4/v-museum/src/app/app.module.ts
+++ Angular_lte_4/v-museum/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppMuseumIntroComponent } from './app-museum-intro/app-museum-intro.component';
+import { SideNavBarComponent } from './components/side-nav-bar/side-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMuseumIntroComponent,
+    SideNavBarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
+  bootstrap: [AppMuseumIntroComponent]
 })
 export class AppModule { }
