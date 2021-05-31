96a99f7f566e43b27209244876f4493cac4d3bba
steadyshan <steadyshan@gmail.com>
2021-05-31 12:20:04 -0700

    nav working for gallery

--- Angular_lte_4/v-museum/src/app/components/viewer/viewer.component.ts
+++ Angular_lte_4/v-museum/src/app/components/viewer/viewer.component.ts
@@ -1,3 +1,4 @@
+import { CompileMetadataResolver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 // import * as fs from 'fs';
@@ -14,7 +15,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent implements OnInit {
-  imageList:string[] = [
+  imageListX:string[] = [
     `assets/all-images/starters/intro-Ganesh.jpg`,
     `assets/all-images/starters/Aumkar-Ganesh-sept-2020-final.jpeg`,
     `assets/all-images/starters/AumGaShivParKashiVishwCTLightNov172020Final.jpeg`,
@@ -23,12 +24,111 @@ export class ViewerComponent implements OnInit {
 
 
   ];
-  currentImage =  `assets/all-images/starters/intro-Ganesh.jpg`;
+  selectedImageList:string[] = [ `assets/all-images-demo/starters/su-30-1.jpeg`,
+  `assets/all-images-demo/starters/train1.jpeg`,
+  `assets/all-images-demo/starters/boat1.jpeg`,
+  `assets/all-images-demo/starters/car1.jpeg`,
+  `assets/all-images-demo/starters/dc-10-1.jpeg`,
+  `assets/all-images-demo/starters/train2.jpeg`,
+  `assets/all-images-demo/starters/ship1.jpeg`,
+  `assets/all-images-demo/starters/car2.jpeg`,];
+  imageList2:any[] = [
+   { 
+    folder:'starters',
+    files: [`su-30-1.jpeg`,
+            `train1.jpeg`,
+            `boat1.jpeg`,
+            `car1.jpeg`,
+            `dc-10-1.jpeg`,
+            `train2.jpeg`,
+            `ship1.jpeg`,
+            `car2.jpeg`,
+          ]
+   }, { 
+    folder:'planes',
+    files: [`su-30-1.jpeg`,
+    `dc-10-1.jpeg`,
+          ]
+   },{ 
+    folder:'trains',
+    files: [`train1.jpeg`,
+    `train2.jpeg`]
+   }, { 
+    folder:'ships',
+    files: [`boat1.jpeg`,
+            `ship1.jpeg`,
+          ]
+   }, { 
+    folder:'cars',
+    files: [`car1.jpeg`,
+            `car2.jpeg`,
+          ]
+   },
+
+  ];
+  imageList:any[] = [
+    { 
+     folder:'starters',
+     files: [`intro-Ganesh.jpg`,
+             `Aumkar-Ganesh-sept-2020-final.jpeg`,
+             `AumGaShivParKashiVishwCTLightNov172020Final.jpeg`,
+             `unsignedShivVeena12272020.jpeg`,
+             `ShreeSiddhivinayakQ42020.jpeg`,
+           ]
+    },{ 
+      folder:'starters-2',
+      files: [`su-30-1.jpeg`,
+              `train1.jpeg`,
+              `boat1.jpeg`,
+              `car1.jpeg`,
+              `dc-10-1.jpeg`,
+              `train2.jpeg`,
+              `ship1.jpeg`,
+              `car2.jpeg`,
+            ]
+     }, { 
+     folder:'planes',
+     files: [`su-30-1.jpeg`,
+     `dc-10-1.jpeg`,
+           ]
+    },{ 
+     folder:'trains',
+     files: [`train1.jpeg`,
+     `train2.jpeg`]
+    }, { 
+     folder:'ships',
+     files: [`boat1.jpeg`,
+             `ship1.jpeg`,
+           ]
+    }, { 
+     folder:'cars',
+     files: [`car1.jpeg`,
+             `car2.jpeg`,
+           ]
+    },
+ 
+   ];
+  currentImage =  this.selectedImageList[0]; // `assets/all-images-demo/starters/su-30-1.jpeg`;
   constructor(private activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(params=> { console.log(JSON.stringify(params))});
+    let themed:any = null ;
+    let foundList:any = null ;
+    this.activatedRoute.paramMap.subscribe(param => 
+      { 
+        themed = param; console.log(themed.params.theme);
+        foundList = this.imageList.filter(x => x.folder === themed.params.theme.toString());
+        if (foundList !== null && foundList.length > 0) {
+          let foundFolder = foundList[0].folder;
+          this.selectedImageList = [];
+          foundList[0]
+              .files
+              .forEach( fileName => this.selectedImageList.push(`assets/all-images/${foundFolder}/${fileName}`));
+          }
+          this.currentImage = this.selectedImageList[0];
+      });
     
+  
   }
 
   showFullSize(source:string) {
--- Angular_lte_4/v-museum/src/app/services/categories.service.ts
+++ Angular_lte_4/v-museum/src/app/services/categories.service.ts
@@ -11,15 +11,24 @@ export class CategoriesService {
   constructor() { }
   get Categories(): ParamElement[] {
     return [
-       { label: 'Starter', key:'starters'},
-       { label: 'Ganesh, Shiva Parvati', key:'dietyGaneshShivParvati'},
-       { label: 'Vishnu, Laxmi, Hanuman', key:'dietyVishnuLaxmiHanuman'},
-       { label: 'Sadgurus ', key:'sSadgurus'},
-       { label: 'Shirdi Sai', key:'sShirdiSai'},
-       { label: 'Living subjects', key:'livingSubjects'},
-       { label: 'Inanimate subjects', key:'inanimateSubjects'},
+      { label: 'Starter', key:'starters'},
+      { label: 'Transport', key:'starters2'},
+      { label: 'Air', key:'planes'},
+      { label: 'Rail', key:'trains'},
+      { label: 'Sea', key:'ships'},{ label: 'Automobiles', key:'cars'},
+      
 
        { label: 'Specials', key:'specials'},
-            ];
+            ];/*return [
+              { label: 'Starter', key:'starters'},
+              { label: 'Ganesh, Shiva Parvati', key:'dietyGaneshShivParvati'},
+              { label: 'Vishnu, Laxmi, Hanuman', key:'dietyVishnuLaxmiHanuman'},
+              { label: 'Sadgurus ', key:'sSadgurus'},
+              { label: 'Shirdi Sai', key:'sShirdiSai'},
+              { label: 'Living subjects', key:'livingSubjects'},
+              { label: 'Inanimate subjects', key:'inanimateSubjects'},
+       
+              { label: 'Specials', key:'specials'},
+                   ];*/
   } 
 }
