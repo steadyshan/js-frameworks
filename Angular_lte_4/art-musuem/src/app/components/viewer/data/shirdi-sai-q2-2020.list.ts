121c843a4be6c52693cf4a2ea85abf4c4ead4ab4
steadyshan <steadyshan@gmail.com>
2021-06-05 22:38:56 -0700

    image and minor updates, mainly Shirdi Sai

--- Angular_lte_4/art-musuem/src/app/components/viewer/data/shirdi-sai-q2-2020.list.ts
+++ Angular_lte_4/art-musuem/src/app/components/viewer/data/shirdi-sai-q2-2020.list.ts
@@ -15,34 +15,84 @@ public allImageList:ImageElement[]  =  [
         folder:'shirdi-sai-q1-q2-2021',
         theme:'Shirdi Sai Sketches Q1 and Q2 2021',
         themeSummary:
-        ``,
-        files: [  {  fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaFaceTemplate4152021.jpg`, 
+        `<p> <em><strong>Soo many sketches of Shirdi Sai!!!</strong> that I needed to make a separate section with specific period for the last Datta Avatar, Sadguru Shree Sai Samartha</em><br/>
+        &nbsp;&nnbsp; * Of late (June 2021) I have been attempting on sketch every Thursday. Let's see how far this sustains... `,
+        files: [ {  fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/BabaonStoneFamousPic.jpeg`, 
+        description: `<b> AUM SAI RAM</b>` 
+            
+            },{  fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasBaba05272021_all.jpeg`, 
+            description: `<ul><li><strong> Milpitas CA Temple Sai</strong></li>
+                    <li>Attempt to sketch Ganesh and all of Baba's representations in the Milpitas Temple, except the palkhi and grand portrait at the entrance</li>
+                    <li>Left: Shree Ganesh and Chavadi Baba, Right main hall Main idol (top) and small idol, and center Dwarkamai painting.</li>
+
+                    <li>May 27 2021 - added in the begining to attempt introducing the temple deities</li>
+                    <li>Temple address 1221 California Circle, Milpitas CA  </ul>` 
+                
+                }, {  fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaFaceTemplate4152021.jpg`, 
             description: `<ul>
-                    <li>Based off of Shirdi Sai Temple Milpitas, CA main idol face, I use this as a 'template' to draw Baba's face with different 'decorations'</li>
+            <li><strong> Milpitas CA Temple Sai</strong></li>
+            <li>Based off of Shirdi Sai Temple Milpitas, CA main idol face, I use this as a 'template' to draw Baba's face with different 'decorations'</li>
                     <li>April 15 2021</li> </ul>` 
                 
                 },{  fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaFullTemplateQ22021.jpg`, 
                 description: `<ul>
-                        <li>Like the previous, based off of Shirdi Sai Temple Milpitas, CA main idol face, I use this as a 'template' to draw Baba's face with different 'decorations'</li>
+                <li><strong> Milpitas CA Temple Sai</strong></li>
+                <li>Like the previous, based off of Shirdi Sai Temple Milpitas, CA main idol face, I use this as a 'template' to draw Baba's face with different 'decorations'</li>
                         <li>April - May 2021</li> </ul>` 
                     
-                    },
+                    }, {  
+                        fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/BabaMainHallTemplateQ1QQ2021.jpg`, 
+                        description: `
+                           <ul>
+                           <li><strong> Milpitas CA Temple Sai</strong></li>
+                    <li>Milpitas Baba Main Hall 'template', showing the main and small 'murthies' the tulsi, Sai Yantram, pipe, etc and the back</li>
+                           <li>After the Kakad Aarti, Thursdays through Sundays, there would normally be  a throne installed</li>
+                           <li>However, since the start of the COVID-19 (March 2020) through the time of writing this (start JUne 2021), throne is been installed
+                           only very select festivals.</li>
+                           <li>Drawing the throne is an elaborate undertaking; when I have the flexibility and patience :) I'll get down to sketch that theme</li>
+                           </ul>
+                           ` },
             {  
             fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaAprilMay2021.jpg`, 
             description: `
-               <ul><li>The first Baba sketch based off of one of the three templates I created, this is Milpitas Temple Baba portrait.</li>
+               <ul><li><strong> Milpitas CA Temple Sai</strong></li>
+               <li>The first Baba sketch based off of one of the three templates I created, this is Milpitas Temple Baba portrait.</li>
+               <li>Probably April 22 2021</li></ul>
+               ` },  {  
+                fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaAprilMay2021_2.jpg`, 
+                description: `
+                    <ul><li><strong> Milpitas CA Temple Sai</strong></li>
+                    <li>Baba sketch based off one of the three templates I created, this is Milpitas Temple Baba portrait.</li>
+                    <li>Probably April 29 2021</li></ul>
+                   ` } ,  {  
+                    fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasBaba05202021_full.jpeg`, 
+                    description: `
+                       <ul><li><strong> Milpitas CA Temple Sai</strong></li>
+                       <li>May 20, 2021</li></ul>
+                       ` }, {  
+                    fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/Milpitas-Baba-June3-2021.jpeg`, 
+                    description: `
+                       <ul><li><strong> Milpitas CA Temple Sai</strong></li>
+                       <li>Based of one of the three templates I created, this is Milpitas Temple Baba with night robes..</li>
+                       <li>June 3 2021 - big idol head scarf was light almost light green, shawl is red, </li>
+                       <li>small idol light creamish robes</li></ul>
+                       ` }
+                ,
+            {  
+            fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaAprilMay2021.jpg`, 
+            description: `
+               <ul><li><strong> Milpitas CA Temple Sai</strong></li>
+               <li>The first Baba sketch based off of one of the three templates I created, this is Milpitas Temple Baba portrait.</li>
                <li>Probably April 22 2021</li></ul>
                ` },  {  
                 fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasMainBabaAprilMay2021_2.jpg`, 
                 description: `
                     <ul><li>Baba sketch based off one of the three templates I created, this is Milpitas Temple Baba portrait.</li>
                     <li>Probably April 29 2021</li></ul>
-                   ` } , {  
-                    fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/Milpitas-Baba-June3-2021.jpeg`, 
+                   ` } ,  {  
+                    fullFileName: `assets/all-images/religion/shirdi-sai-q1-q2-2021/MilpitasBaba05202021_full.jpeg`, 
                     description: `
-                       <ul><li>Based of one of the three templates I created, this is Milpitas Temple Baba with night robes..</li>
-                       <li>June 3 2021 - big idol head scarf was light almost light green, shawl is red, </li>
-                       <li>small idol light creamish robes</li></ul>
+                       <ul><li>May 20, 2021</li></ul>
                        ` }
                     
               ]
