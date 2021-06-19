03c3c16d689b30d2bf42d9a16da55c166fbbd7a1
steadyshan <steadyshan@gmail.com>
2021-06-19 09:15:08 -0700

    changes to viewer styling, added personility sketches

--- Angular_lte_4/art-musuem/src/app/components/views-and-lists/data/people.image.list.ts
+++ Angular_lte_4/art-musuem/src/app/components/views-and-lists/data/people.image.list.ts
@@ -3,46 +3,67 @@ import { PicDescr, ImageElement } from './image.list'
 export class PeopleImageList {
     sort: string = 'asc'
     public allImageList:ImageElement[]  =  [
-       /*
-      /Users/shantanu/Documents/GitHub/js-frameworks/Angular_lte_4/art-musuem/src/assets/all-images/people-places/
-/Users/shantanu/Documents/GitHub/js-frameworks/Angular_lte_4/art-musuem/src/assets/all-images/people-places/
-/Users/shantanu/Documents/GitHub/js-frameworks/Angular_lte_4/art-musuem/src/assets/all-images/people-places/MalcolmM04192021.jpg
-/Users/shantanu/Documents/GitHub/js-frameworks/Angular_lte_4/art-musuem/src/assets/all-images/people-places/ */
-              { 
-             folder:'people-places',
-             theme:'Famous Persons',
-             themeSummary:
-             ``,
-             files: [
-            {fullFileName: `assets/all-images/people-places/ShivajiJayantiTithi03312021.jpeg`, 
-             description: '<p>MAHARAJ!!!! Shiv Jayanti March 31 2021</p>' },
-{fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021-color.jpg`, 
-             description: '<p>Maharaj on horseback attempt based on Masunda Lake, Thane statue</p>',
-  iterations:[
-     {fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021-color.jpg`, 
-     description: `<ul><li>Maharaj on horseback </li><li>Attempt based on Masunda Lake, Thane statue.</li>
-                    <li>Color pencil attempt - Shiv jayanti 3/31/2021</li>
-                    <li><em>(Still WIP technically)</em></li></ul>`},
 
-     {fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021.jpeg`, 
-     description: `<ul><li>Maharaj on horseback </li><li>Black and white</li></ul>`}
-  ] },
-{fullFileName: `assets/all-images/people-places/UnsignedWIPPtBhimsenJoshiJan242021.jpeg`, 
-             description: `<ul><li>BHARAT RATNA Pt. Bhimsen Joshi </li>
+{ 
+    folder:'people-places',
+    theme:'Famous Persons',
+    themeSummary:
+      `Faces, figures sketching started almost at the start of my 'pandemic caused' reconnect with this hobby
+         Some off the spur of the moment, spontaneous, others due to their deaths (and we have had a lot of that!!)
+         Some, in fact, many are <em><strong>pretty out of shape</strong></em>, but <em>still</em> included because of the significance`,
+     files: [
+         {
+            fullFileName: `assets/all-images/people-places/ShivajiJayantiTithi03312021.jpeg`, 
+            description: `<ul>
+                           <li>MAHARAJ!!!! </li>
+                           <li>One of my unquestioned heroes - <br/>
+                                 <em>(if i were to be reborn given a choice of 'when' and 'as what', it would be as one of Shivaji's Mavlas)</em></li>
+                           <li> sketched on Shiv Jayanti March 31 2021. One of my</li>
+                          </ul>`
+         },
+         {
+            fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021-color.jpg`, 
+            description: '<p> Maharaj on horseback attempt based on Masunda Lake, Thane statue</p>',
+            iterations:[ 
+                  {
+                     fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021-color.jpg`, 
+                     description: `<ul
+                                    <li>Maharaj on horseback </li>
+                                    <li>Attempt based on Masunda Lake, Thane statue.</li>
+                                    <li>Color pencil attempt - Shiv jayanti 3/31/2021</li>
+                                    <li><em>(Still WIP technically)</em></li>
+                                   </ul>`
+                  },
+                  {
+                     fullFileName: `assets/all-images/people-places/ShivbaHorseTithiJayanti_wip_03_31_2021.jpeg`, 
+                     description: `<ul><li>Maharaj on horseback </li><li>Black and white</li></ul>`}
+                        ] 
+         },
+         {
+            fullFileName: `assets/all-images/people-places/UnsignedWIPPtBhimsenJoshiJan242021.jpeg`, 
+            description: `<ul>
+                              <li>BHARAT RATNA Pt. Bhimsen Joshi </li>
                               <li>Sketched on Jan 24 2021 His 10th death anniversary</li>
-                              <li>BIG FAN OF HIS... Also his centennial anniversay is this (DOB. Feb 4 2022)</li></ul>`,
-                              iterations:[
-                                 {fullFileName: `assets/all-images/people-places/UnsignedWIPPtBhimsenJoshiJan242021.jpeg`, 
-                                 description: `<ul><li>BHARAT RATNA Pt. Bhimsen Joshi </li>
-                                 <li>Sketched on Jan 24 2021 His 10th death anniversary</li>
-                                 <li>BIG FAN OF HIS... Also his centennial anniversay is this (DOB. Feb 4 2022)</li></ul>`},
-                            
-                                 {fullFileName: `assets/all-images/people-places/UnsignedPortraitPtBhimsenJoshiJan242021.jpeg`, 
-                                 description: `<ul><li>Close up/li></ul>`}
-                              ] },
-                              {fullFileName: `assets/all-images/people-places/PtBhimsenJoshi06142021-3.jpeg`, 
-                              description: '<p>Maharaj on horseback attempt based on Masunda Lake, Thane statue</p>',
-                   iterations:[
+                              <li>BIG FAN OF HIS... Also his centennial anniversay is this (DOB. Feb 4 2022)</li>
+                          </ul>`,
+            iterations:[
+               {
+                  fullFileName: `assets/all-images/people-places/UnsignedWIPPtBhimsenJoshiJan242021.jpeg`, 
+                  description: `<ul>
+                                    <li>BHARAT RATNA Pt. Bhimsen Joshi </li>
+                                    <li>Sketched on Jan 24 2021 His 10th death anniversary full sketch</li>
+                                    <li>BIG FAN OF HIS... Also his centennial anniversay is this (DOB. Feb 4 2022)</li>
+                                 </ul>`
+               },
+               {
+                  fullFileName: `assets/all-images/people-places/UnsignedPortraitPtBhimsenJoshiJan242021.jpeg`, 
+                  description: `<ul><li>Close up/li></ul>`}
+            ] 
+         },
+         {
+            fullFileName: `assets/all-images/people-places/PtBhimsenJoshi06142021-3.jpeg`, 
+            description: '<p>Tuning the Veena</p>',
+            iterations:[
                       {fullFileName: `assets/all-images/people-places/PtBhimsenJoshi06142021-3.jpeg`, 
                       description: `<ul><li>June 13-14 2021 - photo posted on my RAIT group by Vinu, another Bhimsen Fan</li>
                                         <li>Inspired to sketch</li></ul>`},
@@ -53,11 +74,22 @@ export class PeopleImageList {
                       {fullFileName: `assets/all-images/people-places/PtBhimsenJoshi06142021-start.jpeg`, 
                       description: `<ul><li>June 13-14 2021 - start</li></ul>`}
                    ] },
-                   {fullFileName: `assets/all-images/people-places/PtJiQ12021.jpg`, 
-             description: `<ul><li>BHARAT RATNA Pt. Bhimsen Joshi 'practise' sketch before the one on Jan 24</li>` },
-                   {fullFileName: `assets/all-images/people-places/UnsignedWIPActressNutanAttemptJan2021.jpeg`, 
-                   description: '<p>Attempt to sketch Nutan Jan 2021</p>' },
-                   {fullFileName: `assets/all-images/people-places/UnsignedWIPBalasaheb_NetajiJan232021.jpeg`, 
+         {
+            fullFileName: `assets/all-images/people-places/PtJiQ12021.jpg`, 
+            description: `<ul><li>BHARAT RATNA Pt. Bhimsen Joshi 'practise' sketch before the one on Jan 24 2021</li></ul>` 
+         },
+         {
+            fullFileName: `assets/all-images/people-places/GandhiShastri20202.jpg`, 
+            description: `<ul>
+                           <li>Mahatma Gandhi, Lal Bahadur on their birth anniversary, which is the same date Oct 2</li>
+                           <li>Gandhi was born in 1869, Shastri in 1904 and this sketch in 2020 - (lol! span 3 centuries)</li>
+                          </ul>` 
+         },
+         {
+            fullFileName: `assets/all-images/people-places/UnsignedWIPActressNutanAttemptJan2021.jpeg`, 
+            description: '<p>Attempt to sketch Nutan Jan 2021</p>' 
+         },
+         {fullFileName: `assets/all-images/people-places/UnsignedWIPBalasaheb_NetajiJan232021.jpeg`, 
              description: '<p>Bal Thackeray and Netaji attempt - their birth anniv Jan 23 2021</p>' },,
                           {fullFileName: `assets/all-images/people-places/JohnLennon.jpg`, 
                                        description: '<p>Quick Lennon sketch <em>Think</em> Q1 2021</p>' },
@@ -68,4 +100,14 @@ export class PeopleImageList {
              ]
           },
      ]
+
+ /*
+ VanHalenOct62020.jpg
+ AndersonOct2020.jpg
+ BigBOct2020.jpg
+ ConneryOct312020.jpg
+ GandhiShastri20202.jpg
+ 
+
+ */             
 }
