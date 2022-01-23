a5a8646962e827e430e1d949b230753ffb786c29
steadyshan <steadyshan@gmail.com>
2022-01-23 08:45:59 -0800

    completion of Mumbai theme individual sketch add

--- Angular_lte_4/shan-cms-template/src/assets/data-and-config/data/mumbai-meri-jaan.list.ts
+++ Angular_lte_4/shan-cms-template/src/assets/data-and-config/data/mumbai-meri-jaan.list.ts
@@ -55,12 +55,115 @@ public allImageList:ImageElement[]  =  [
               },  { 
                 fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme26112020_finalVer1.png`,
                 description: `<ul>
+                <li>26/11 Tribute - the complete compilation</li>
                 <li>..continuing from the 1st sketch, compilation which is 9 A4 size sketches 'stitched' together.. </li>
                 <li>click the '>>' button above to see the (other) individual pieces in more detail</li>
                 <li>Nov 2020 timeframe</li>
               </ul>`,
                 dateUploaded: '01-31-2022',
-            },]
+            },  { 
+              fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme1_GodsNov2020.jpeg`,
+              description: `<ul>
+              <li>Top left corner piece</li>
+              <li>Shree Siddhivinayak Prabhadevi</li>
+              <li>Shree Kashi Visheshwar sculpture overlooking main temple Citylight </li>
+              <li>Mahakali, Mahalaxmi, MahaSarwaswati - Mahalaxmi temple, Mahalaxmi</li>
+              <li>Shivling - Babulnath</li>
+              <li>Shree Swami Samartha - matth -  Shivaji Park</li>
+              <li>Gonya Maruti Temple - Dadar East(??)</li>
+              <li>Nov 2020 timeframe</li>
+            </ul>`,
+              dateUploaded: '01-31-2022',
+          },  { 
+            fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme2_PlacesofWorshipNov2020.jpeg`,
+            description: `<ul>
+            <li>Top center piece</li>
+            <li>Haji Ali Temple, Haji Ali</li>
+            <li>Buddhist Stupa, Shivaji Park </li>
+            <li>Parsi Fire temple (unknown)</li>
+            <li>Mount Mary Bandra</li>
+            <li>Nov 2020 timeframe</li>
+          </ul>`,
+            dateUploaded: '01-31-2022',
+        } , { 
+          fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme3_LifelinesNov2020.jpeg`,
+          description: `<ul>
+          <li>Top right piece</li>
+          <li>Mumbai Local</li>
+          <li>BEST buses</li>
+          <li>Taxi</li>
+          <li>Auto</li>
+          <li>(center) 'Dabbawala haathgadi'</li>
+        </ul>`,
+          dateUploaded: '01-31-2022',
+      }, { 
+        fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme4_SocialGathering.jpeg`,
+        description: `<ul>
+        <li>Center left piece</li>
+        <li>Reopublic Day Marchpast shivaji park</li>
+        <li>Id greetings</li>
+        <li>Christmas Mass procession</li>
+        <li>Navy Ball Navinagar(??)</li>
+        <li>Ganapati Visarjan - lalbaugh</li>
+      </ul>`,
+        dateUploaded: '01-31-2022',
+    }, { 
+      fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme5_FoodieNov2020.jpeg`,
+      description: `<ul>
+      <li>Center right piece - food</li>
+      <li>Kulfi with falooda</li>
+      <li>Panipuri/Bhelpuriwala</li>
+      <li>Dosa</li>
+      <li>Wadapav and 'cutting chai'</li>
+      <li>Jalebi</li>
+      <li>Wine and Plum cake</li>
+      <li>Pav bhaji</li>
+      <li>Typical Thali</li>
+      <li>Biryani, Seekh kabab and Tandoori</li>
+      <li>Falooda</li>
+      <li>Fish Patra</li>
+    </ul>`,
+      dateUploaded: '01-31-2022',
+  }, { 
+    fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme6_MumbaiSitesOldNov2020.jpeg`,
+    description: `<ul>
+      <li>Bottom Left piece - old and cultural</li>
+      <li>Mahim fort - TV tower and twin towers worli in the distance</li>
+      <li>Trimurti - Elephanta caves</li>
+      <li>Kanheri caves (??)</li>
+      <li>Gateway of India, Taj hotel, waterfront with launches</li>
+      <li>'Foutain' statue and Rajabai clock tower</li>
+      <li>Victoria Terminus</li>
+      <li>Muncipal building (opp VT)</li>
+      <li>High court</li>
+  </ul>`,
+    dateUploaded: '01-31-2022',
+}, { 
+  fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme7_MumbaiSitesMidNov2020.jpeg`,
+  description: `<ul>
+  <li>Bottom Center piece 'middle age' architecture</li>
+  <li>TV Tower</li>
+  <li>Nehru Planetarium</li>
+  <li>Navi Lions Gate</li>
+  <li>Trade exchange building</li>
+  <li>Marine drive flyover</li>
+  <li>Wankhede Stadium</li>
+  <li>707 replica for kids - Juhu Beach</li>
+  <li>Raja Shivaji statue Shivaji Park</li>
+</ul>`,
+  dateUploaded: '01-31-2022',
+}, { 
+  fullFileName: `assets/all-images/mumbai-meri-jaan/MumbaiTheme8_MumbaiTodayNov2020.jpeg`,
+  description: `<ul>
+  <li>Bottom Right and last piece Mumbai New Look!!</li>
+  <li>Airport International Terminal</li>
+  <li>(2022 tallest in mumbai) Imperial Tower, Tardeo</li>
+  <li>Bandra-Worli Sea link</li>
+  <li>Monorail</li>
+  <li>Metro</li>
+</ul>`,
+  dateUploaded: '01-31-2022',
+},]
         },
     ]
 }
