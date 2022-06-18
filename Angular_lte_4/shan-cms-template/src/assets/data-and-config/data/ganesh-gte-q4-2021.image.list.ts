694370dbb1d7f126d6322d7dae004505910d1813
steadyshan <steadyshan@gmail.com>
2022-06-17 23:37:58 -0700

    Ganesh image

--- Angular_lte_4/shan-cms-template/src/assets/data-and-config/data/ganesh-gte-q4-2021.image.list.ts
+++ Angular_lte_4/shan-cms-template/src/assets/data-and-config/data/ganesh-gte-q4-2021.image.list.ts
@@ -51,11 +51,17 @@ public allImageList:ImageElement[]  =  [
                            evolutionDate: '02-06-2022',
                            dateUploaded: '06-17-2022', iterations:[
                { 
-                  fullFileName: `assets/all-images/religion/shree-ganesh-gte-q4-2021/GaneshFeb52022SaraswatiPooja1.jpeg`, 
+                  fullFileName: `assets/all-images/religion/shree-ganesh-gte-q4-2021/GaneshJune172022_chaturthi.jpeg`, 
                   description: `<strong><ul>
                                     <li>Feb 5 2022 Saraswati Pooja - my first attempt using crayons</li>
                                     <li>Also a dedication to a very sad event - the passing of Lata Mangeshkar</li>
-                                 </ul></strong>`,}],
+                                 </ul></strong>`,},
+                                 { 
+                                    fullFileName: `assets/all-images/religion/shree-ganesh-gte-q4-2021/GaneshJune172022_chaturthi_prev.jpeg`, 
+                                    description: `<strong><ul>
+                                                      <li>Feb 5 2022 Saraswati Pooja - my first attempt using crayons</li>
+                                                      <li>Also a dedication to a very sad event - the passing of Lata Mangeshkar</li>
+                                                   </ul></strong>`,}       ],
          },
          {
             fullFileName: `assets/all-images/religion/shree-ganesh-gte-q4-2021/GaneshFeb52022SaraswatiPooja1.jpeg`, 
