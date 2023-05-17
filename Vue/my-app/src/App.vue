5f171123beb6fb93a74a3ab9918e17667db0a245
steadyshan <steadyshan@gmail.com>
2023-05-17 16:18:47 -0700

    vuew app

---
+++ Vue/my-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Timeline from './components/Timeline.vue';
+</script>
+
+<template style="">
+  <div class="section" style="vertical-align: top; padding: 0px;border: 2px red inset">
+    <div class="container">
+      <Timeline  style="width: 100%; " ></Timeline>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+</style>
---
+++ Vue/my-app/src/components/Timeline.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+  const periods = ["Today","This Week","This Month"];
+  let currentSelected = ref("Today");
+  function selectPeriod(period:string){
+    currentSelected.value = period ;
+    switch (period) {
+        case "Today": currentSelected.value = `${currentSelected.value} ${new Date().toLocaleDateString()}` ;break 
+
+        case "This Week": currentSelected.value =  `${currentSelected.value} ${new Date().getDay() }` ;break  ; 
+
+        
+    }
+  }
+</script>
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs" style="width:100%; background-color: aqua;">
+          <div style="width:100%; background-color: aqua; " :key="period" v-for="period of periods">
+            <a  v-on:click="selectPeriod(period)">{{ period }}</a><br/>
+          </div>
+          <br/>
+          
+       <!-- <a :key="period" v-for="period of periods">{{ period }}</a> -->
+        </span>
+        <div>{{ currentSelected }}</div>
+       
+    </nav>  
+</template>
+<style>
+</style>
